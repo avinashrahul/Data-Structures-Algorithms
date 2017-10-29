d31c747cd08bb44d0eed76279ee71f21ae4eea72
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-10-29 13:55:48 -0600

    btree

---
+++ binary_search_tree.rb
@@ -0,0 +1,100 @@
+require 'pry'
+class Node
+  attr_accessor :data, :left, :right
+
+  def initialize(data = nil)
+    @data  = data
+    @left  = nil
+    @right = nil
+  end
+
+  def insert(node)
+    if data.nil?
+      self.data = node.data
+      return  self
+    end
+
+    if node.data < data
+      self.left = Node.new if left.nil?
+      self.left.insert(node)
+    elsif node.data > data
+      self.right = Node.new if right.nil?
+      self.right.insert(node)
+    end
+  end
+
+  def search(node_data)
+    return nil if data.nil?
+
+    if node_data == data
+      return self
+    elsif node_data < data
+      left.search(node_data)
+    elsif node_data > data
+      right.search(node_data)
+    end
+  end
+
+  def destroy(node_data)
+    if node = search(node_data)
+      destroy_node(node)
+    end
+  end
+
+  # def to_a(mode = :preorder)
+  #   return nil if data.nil?
+
+  #   method_name = 'to_a_' + (MODES.include?(mode) ? mode : :preorder).to_s
+  #   send(method_name)
+  # end
+
+  protected
+
+  def destroy_node(node)
+    destroy_node(node.left) unless node.left.nil?
+    destroy_node(node.right) unless node.right.nil?
+
+    node.data = node.left = node.right = nil
+  end
+
+  # Pre-order displays root node, left node and then right node
+  # def to_a_preorder
+  #   tree = []
+  #   tree << data
+  #   tree << left.to_a_preorder unless left.nil?
+  #   tree << right.to_a_preorder unless right.nil?
+
+  #   tree
+  # end
+
+  # In-order displays left node, root node and then right node
+  # def to_a_inorder
+  #   tree = []
+  #   tree << left.to_a_inorder unless left.nil?
+  #   tree << data
+  #   tree << right.to_a_inorder unless right.nil?
+
+  #   tree
+  # end
+  #
+  # Post-order displays left node, root node and then right node
+  # def to_a_postorder
+  #   tree = []
+  #   tree << left.to_a_postorder unless left.nil?
+  #   tree << right.to_a_postorder unless right.nil?
+  #   tree << data
+
+  #   tree
+  # end
+end
+
+tree = Node.new
+tree.insert(Node.new(5))
+tree.insert(Node.new(10))
+tree.insert(Node.new(4))
+tree.insert(Node.new(2))
+tree.insert(Node.new(12))
+puts tree.search(4).inspect
+puts tree.destroy(4).inspect
+puts tree.search(5).inspect
+puts tree.inspect
